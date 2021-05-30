2f09934da2e968ea2f9a62952f9ccbe7eacbf501
Christian Duffau-Rasmussen <cd@ante.dk>
2021-05-30 12:44:45 +0200

    start/end test

--- tests/integration/test_baby_tracker.py
+++ tests/integration/test_baby_tracker.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import re
 
 SERVER_IP = os.getenv("SERVER_IP")
 SERVER_PORT = os.getenv("SERVER_PORT")
@@ -33,9 +34,53 @@ def test_create_list_delete_feed_record():
     print(resp.json())
     assert "error" not in resp.json()["text"]
 
+def test_start_end_feed():
+    params = {"text": "s 12:30"}
+    resp = requests.post(BASE_URL + "/feed", data=params)
+    assert resp.status_code == 200
+    print(resp.json())
+    assert "error" not in resp.json()["text"]
 
-def test_create_sleep_record():
-    params = {"text": "12:30 14:30"}
+    params = {"text": "e 12:35"}
+    resp = requests.post(BASE_URL + "/feed", data=params)
+    assert resp.status_code == 200
+    print(resp.json())
+    assert "error" not in resp.json()["text"]
+    assert "00:05" in resp.json()["text"]
+
+
+def test_create_list_delete_sleep_record():
+    params = {"text": "12:30 12:35"}
     resp = requests.post(BASE_URL + "/sleep", data=params)
     assert resp.status_code == 200
+    print(resp.json())
     assert "error" not in resp.json()["text"]
+
+    params = {"text": "list"}
+    resp = requests.post(BASE_URL + "/sleep", data=params)
+    assert resp.status_code == 200
+    print(resp.json())
+    assert "error" not in resp.json()["text"]
+    assert "12:30" in resp.json()["text"]
+    assert "00:05" in resp.json()["text"]
+
+    params = {"text": "d 1"}
+    resp = requests.post(BASE_URL + "/sleep", data=params)
+    assert resp.status_code == 200
+    print(resp.json())
+    assert "error" not in resp.json()["text"]
+
+
+def test_start_end_sleep():
+    params = {"text": "s 12:30"}
+    resp = requests.post(BASE_URL + "/sleep", data=params)
+    assert resp.status_code == 200
+    print(resp.json())
+    assert "error" not in resp.json()["text"]
+
+    params = {"text": "e 12:35"}
+    resp = requests.post(BASE_URL + "/sleep", data=params)
+    assert resp.status_code == 200
+    print(resp.json())
+    assert "error" not in resp.json()["text"]
+    assert "00:05" in resp.json()["text"]
