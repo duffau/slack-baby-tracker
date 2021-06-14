75879a18294d2bb7e7c7a4ba48d1dae2ce3eb37a
Christian Duffau-Rasmussen <cd@ante.dk>
2021-06-14 15:25:07 +0200

    Create duration record takea durarion validation function as input

--- baby_tracker/router/_duration.py
+++ baby_tracker/router/_duration.py
@@ -7,13 +7,21 @@ from baby_tracker.utils import format_duration, format_timestamp, timedelta_to_s
 
 from .config import SLACK_OAUTH_TOKEN, CHANNEL_ID
 
-def create_duration_record(args, create_db_record, db_conn):
+
+def _validate_duration(duration):
+    if duration is not None:
+        assert (
+            timedelta_to_seconds(duration) > 0
+        ), "Duration must be positive. Got from_time '{from_time}' and to_time: '{to_time}' which gives a duration of '{duration}' seconds."
+
+
+def create_duration_record(args, create_db_record, db_conn, validate_duration=_validate_duration):
     from_time, to_time = parse_duration_args(args)
     try:
         duration = to_time - from_time
     except TypeError:
         duration = None
-    _validate_duration(duration)
+    validate_duration(duration)
     return create_db_record(db_conn, (from_time, to_time, duration)), duration
 
 
@@ -25,13 +33,6 @@ def make_duration_status_text(db_conn, table, latest_id=None):
     latest_date, latest_tot_duration = an.latest_daily_total_duration(db_conn, table)
     status_text.append(f"Total duration on {latest_date.strftime('%A %d/%m')}: *{format_duration(latest_tot_duration)}*")
     return "\n".join(status_text)
-
-
-def _validate_duration(duration):
-    if duration is not None:
-        assert (
-            timedelta_to_seconds(duration) > 0
-        ), "Duration must be positive. Got from_time '{from_time}' and to_time: '{to_time}' which gives a duration of '{duration}' seconds."
 
 
 def parse_duration_args(args):
